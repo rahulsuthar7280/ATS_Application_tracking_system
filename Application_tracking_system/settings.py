10b9c11686e57e57a1b63ae8e8491e45ea199908
Rahul Suthar <rahul.suthar@vc-erp.com>
2025-10-11 14:56:06 +0530

    update

--- Application_tracking_system/settings.py
+++ Application_tracking_system/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = BASE_DIR / 'staticfiles' # For production deployment
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
